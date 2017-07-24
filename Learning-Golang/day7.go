2e8f11e61f4d3c9ed7320a857cb8e5762ce00414
manu32 <manjeetthakur@outlook.com>
2017-07-24 21:17:24 +0530

    Day 7: 1

--- Learning-Golang/day7.go
+++ Learning-Golang/day7.go
@@ -6,13 +6,13 @@ import (
 	"fmt"
 )
 
-func Sqrt(x float64) float64{
+func Sqrt(x float64) float64 {
 
 	z := float64(2.)
 	s := float64(0)
 
 	for {
-		z  = z - (z*z - x)/(2*z)
+		z = z - (z*z-x)/(2*z)
 		if math.Abs(s-z) < 1e-15 {
 			break
 		}
